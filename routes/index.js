aecf77b58f5dc7ce83b4ae69a35f9f23c7e80eff
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-18 13:14:31 +0300

    Adding all the routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-import { Router} from 'express';
+import { Router } from 'express';
 import * as AppController from '../controllers/AppController';
 import * as UsersController from '../controllers/UsersController';
 import * as AuthController from '../controllers/AuthController';
