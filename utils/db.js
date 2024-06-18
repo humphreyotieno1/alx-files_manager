26ab39ab9300e3ec9192a55103df726cb8859dc6
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-18 12:15:25 +0300

    MongoDB client

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,17 @@
-import mongodb from 'mongodb';
-import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+const { MongoClient, ObjectId } = require('mongodb');
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || '27017';
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    envLoader();
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const dbURL = `mongodb://${host}:${port}/${database}`;
-
-    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client.connect();
+    this.database = DATABASE;
+    this.host = HOST;
   }
 
   isAlive() {
@@ -19,21 +19,122 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments();
+    try {
+      const db = this.client.db(this.database);
+      const users = db.collection('users');
+      return await users.countDocuments();
+    } catch (err) {
+      console.error('Error counting documents in "users" collection');
+    }
   }
 
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments();
+    try {
+      const db = this.client.db(this.database);
+      const files = db.collection('files');
+      return await files.countDocuments();
+    } catch (err) {
+      console.error('Error counting documents in "files" collection');
+    }
   }
 
-  async usersCollection() {
-    return this.client.db().collection('users');
+  async findFileById(id) {
+    try {
+      const db = this.client.db(this.database);
+      const files = db.collection('files');
+      const results = await files.findOne({ _id: ObjectId(id) });
+      return results;
+    } catch (err) {
+      throw Error('Cant find file by "id"');
+    }
   }
 
-  async filesCollection() {
-    return this.client.db().collection('files');
+  async createNewFile(fileObject) {
+    try {
+      const db = this.client.db(this.database);
+      const files = db.collection('files');
+      return await files.insertOne(fileObject);
+    } catch (err) {
+      throw Error('Cant create new file');
+    }
+  }
+
+  async findFileByUserId(userId) {
+    try {
+      const db = this.client.db(this.database);
+      const files = db.collection('files');
+      return await files.findOne({ userId });
+    } catch (err) {
+      throw Error('Error occured while looking for file with "userId"');
+    }
+  }
+
+  async replaceValue(idObject, updatedObject) {
+    try {
+      const db = this.client.db(this.database);
+      const files = db.collection('files');
+      return await files.replaceOne(idObject, updatedObject);
+    } catch (err) {
+      throw Error('Error occured while updating collection files');
+    }
+  }
+
+  async getUserWithEmail(email) {
+    try {
+      const db = this.client.db(this.database);
+      const users = db.collection('users');
+      const result = await users.findOne({ email });
+      return result;
+    } catch (err) {
+      throw Error('Error occured while finding user in "users" collection');
+    }
+  }
+
+  async getUserWithId(id) {
+    try {
+      const db = this.client.db(this.database);
+      const users = db.collection('users');
+      const result = await users.findOne({ _id: ObjectId(id) });
+      return result;
+    } catch (err) {
+      throw Error('Error occured while finding user in "users" collection');
+    }
+  }
+
+  async createUser(email, hashedPassword) {
+    try {
+      const db = this.client.db(this.database);
+      const users = db.collection('users');
+      await users.insertOne({ email, password: hashedPassword });
+      const user = await users.findOne({ email }, { projection: { _id: 1 } });
+      return user;
+    } catch (err) {
+      throw Error('Error occured while finding user in "users" collection');
+    }
+  }
+
+  async aggregateFiles(userId, parentId, skip, limit) {
+    const pipeline = [
+      {
+        $match: {
+          userId,
+          parentId,
+        },
+      },
+      { $skip: skip },
+      { $limit: limit },
+    ];
+
+    try {
+      const db = this.client.db(this.database);
+      const filesCollection = db.collection('files');
+      const result = await filesCollection.aggregate(pipeline).toArray();
+      return result;
+    } catch (err) {
+      throw new Error('Error aggregating files');
+    }
   }
 }
 
-export const dbClient = new DBClient();
-export default dbClient;
+const dbClient = new DBClient();
+module.exports = dbClient;
