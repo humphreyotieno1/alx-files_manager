ee091f481159706b77da7a37563fef8b42961130
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-18 13:16:00 +0300

    Adding all the routes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,38 +1,61 @@
 import dbClient from '../utils/db';
-import { encryptPassword } from '../utils/crypto';
+import redisClient from '../utils/redis';
+const crypto = require('crypto');
+const bull = require('bull');
 
-export default class UsersController {
-  static async postNew(req, res) {
-    try {
-      const { email, password } = req.body;
-
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
-
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-
-      const exists = await dbClient.getUserWithEmail(email);
-      if (exists) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
-
-      const hashedPassword = encryptPassword(password);
-      const user = await dbClient.createUser(email, hashedPassword);
-      return res.status(201).send({ id: user._id, email: user.email });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send({ error: 'Server error' });
+export async function postNew(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const exists = await dbClient.getUserWithEmail(email);
+    if (exists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    // create queue
+    const queue = new bull('userQueue');
+
+    const hshPassword = encryptPassword(password);
+    const user = await dbClient.createUser(email, hshPassword);
+    queue.add({ userId: user._id });
+    return res.status(201).send({ 'id': user._id, 'email': email });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: 'server error' });
   }
 }
 
-// utils/crypto.js
-export function encryptPassword(password) {
+function encryptPassword(password) {
   const sha1 = crypto.createHash('sha1');
   sha1.update(password);
   const hashedPassword = sha1.digest('hex');
   return hashedPassword;
+}
+
+export async function getMe(req, res) {
+  const token = req.headers['x-token'];
+  if (!token) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+
+  if (!userId) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+
+  const user = await dbClient.getUserWithId(userId);
+
+  if (!user) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+
+  return res.status(200).send({ id: user._id, email: user.email });
 }
